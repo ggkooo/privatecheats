6dd1c26ad6c9b8d962823a62cd0a1e30a346ddcc
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-02-04 10:04:33 -0300

    fix: updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
 import Home from './pages/home'
+import Login from './pages/auth'
 
 export default function App() {
-  return <Home />
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  )
 }
